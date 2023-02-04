8f945b454d1958701cb6f04a1ec838ecce6be9aa
GabrielH4nma <mendesgabriel444@gmail.com>
2023-02-04 17:29:20 +0000

    Sound

--- Assets/Scripts/PlaySound.cs
+++ Assets/Scripts/PlaySound.cs
@@ -7,14 +7,18 @@ public class PlaySounds : MonoBehaviour
 {
     public AudioSource audioSource;
     public Button button;
-    
+    public AudioClip[] musicClips;
+
     private void Start()
     {
-        button.onClick.AddListener(PlaySound);
+        button.onClick.AddListener(PlayMusic);
+        audioSource.playOnAwake = false;
     }
 
-    private void PlaySound()
+    private void PlayMusic()
     {
+        int randomIndex = Random.Range(0, musicClips.Length);
+        audioSource.clip = musicClips[randomIndex];
         audioSource.Play();
     }
 }
